f2b2831cba01014b7fc0021f0b6ae86b8b14e592
jvegax <toledo.utrera@hotmail.com>
2023-03-04 20:23:44 +0100

    chore: add mocked user details page

--- src/pages/user/[username].tsx
+++ src/pages/user/[username].tsx
@@ -1,7 +1,48 @@
+import type { FC } from 'react';
 import React from 'react';
 
-const UserDetails = () => (
-  <div>UserDetails</div>
+import type { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+import type { UserDetails as UserDetailProps } from '@/models/User/types';
+
+interface Props {
+  data: UserDetailProps;
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const users = [
+    { username: 'jvegax' },
+  ];
+
+  const paths = users.map((user) => ({
+    params: { username: user.username },
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps<Props> = async (context: GetStaticPropsContext) => {
+  const username = context.params?.username as string;
+  const response = await fetch(`http://127.0.0.1:3000/api/user/${username}`);
+  const data = await response.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+const UserDetails: FC<Props> = ({ data }) => (
+  <div>
+    <h1>{data.username}</h1>
+    <h1>
+      {data.rating[0].rating}
+      {' '}
+      -
+      {' '}
+      {data.rating[0].ratingType}
+    </h1>
+  </div>
 );
 
 export default UserDetails;

6a1d1a61724a56901df2b9d4e67e46909c93f9e9
Diego <diewentdiaz@gmail.com>
2023-03-11 18:09:13 +0100

    feat: added list of players from a trail

--- src/pages/api/trail/users/[trailId].ts
+++ src/pages/api/trail/users/[trailId].ts
@@ -4,7 +4,7 @@ import type { TrailUser } from '@/models/TrailUser/types';
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<TrailUser>,
+  res: NextApiResponse<TrailUser[]>,
 ) {
   res.status(200).json(MOCK_TRAIL_USER);
 }

0a4e7f6283542557d6c4f118bb3254ba74a87c89
jvegax <toledo.utrera@hotmail.com>
2023-03-14 21:43:05 +0100

    feat: create trail

--- src/components/Trail/TrailCreation/Form/index.tsx
+++ src/components/Trail/TrailCreation/Form/index.tsx
@@ -1,3 +1,6 @@
+'use-client';
+
+import { useCallback } from 'react';
 import {
   Button,
   ButtonRow,
@@ -9,40 +12,74 @@ import {
   Label,
 } from '@/components/Trail/TrailCreation/Form/styles';
 
-const TrailCreationForm = () => (
-  <Form>
-    <FieldContainer>
-      <Label htmlFor="name">Nombre del Trail</Label>
-      <Input type="text" name="name" id="name" />
-    </FieldContainer>
+const TrailCreationForm = () => {
+  const handleSubmit = useCallback(async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-    <FieldContainer>
-      <Label htmlFor="description">Descripción</Label>
-      <InputTextArea name="description" id="description" />
-    </FieldContainer>
+    const form = event.currentTarget;
+    const formData = new FormData(form);
 
-    <DateFieldContainer>
+    const requestData = {
+      name: formData.get('name'),
+      description: formData.get('description'),
+      startDate: formData.get('start-date'),
+      finishDate: formData.get('end-date'),
+      maxPlayers: formData.get('max-players'),
+      owner: '1',
+    };
+
+    console.log({ requestData });
+
+    try {
+      const res = await fetch('https://gametrail-backend-production.up.railway.app/api/trail', {
+        method: 'POST',
+        body: JSON.stringify(requestData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+    } catch (error) {
+      console.log({ corchuelo: error });
+    }
+  }, []);
+
+  return (
+    <Form onSubmit={handleSubmit}>
       <FieldContainer>
-        <Label htmlFor="start-date">Fecha de Inicio</Label>
-        <InputDate type="date" name="start-date" id="start-date" />
+        <Label htmlFor="name">Nombre del Trail</Label>
+        <Input type="text" name="name" id="name" />
       </FieldContainer>
 
       <FieldContainer>
-        <Label htmlFor="end-date">Fecha de Fin</Label>
-        <InputDate type="date" name="end-date" id="end-date" />
+        <Label htmlFor="description">Descripción</Label>
+        <InputTextArea name="description" id="description" />
       </FieldContainer>
-    </DateFieldContainer>
 
-    <FieldContainer>
-      <Label htmlFor="max-players">Número Máximo de Jugadores</Label>
-      <Input type="number" name="max-players" id="max-players" />
-    </FieldContainer>
+      <DateFieldContainer>
+        <FieldContainer>
+          <Label htmlFor="start-date">Fecha de Inicio</Label>
+          <InputDate type="date" name="start-date" id="start-date" />
+        </FieldContainer>
 
-    <ButtonRow>
-      <Button type="submit">Create</Button>
-    </ButtonRow>
+        <FieldContainer>
+          <Label htmlFor="end-date">Fecha de Fin</Label>
+          <InputDate type="date" name="end-date" id="end-date" />
+        </FieldContainer>
+      </DateFieldContainer>
 
-  </Form>
-);
+      <FieldContainer>
+        <Label htmlFor="max-players">Número Máximo de Jugadores</Label>
+        <Input type="number" name="max-players" id="max-players" />
+      </FieldContainer>
+
+      <ButtonRow>
+        <Button type="submit">Create</Button>
+      </ButtonRow>
+    </Form>
+  );
+};
 
 export default TrailCreationForm;

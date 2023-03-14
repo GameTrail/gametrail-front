747525d76c325148bd9d8a77de926208d85cb845
jvegax <toledo.utrera@hotmail.com>
2023-03-14 22:23:52 +0100

    fix: mocks

--- src/models/Trail/mock.ts
+++ src/models/Trail/mock.ts
@@ -7,7 +7,7 @@ export const MOCK_TRAIL: Trail = {
   name: 'Rampas y Chill',
   description: 'Vamos a jugar a algunos juegos de skate, de tranquis y sin presiones. Gente agradable y que esté dispuesta a charlar un buen rato mientras hacemos unas rampitas',
   startDate: '2021/08/01',
-  endDate: '2021/08/01',
+  finishDate: '2021/08/01',
   maxPlayers: 4,
 };
 

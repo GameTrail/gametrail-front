edef061ab47b31eaf6c8595689c4628533ac6789
Josmarlop16 <josmarlop16@alum.us.es>
2023-03-19 16:47:35 +0100

    chore: avances en la navbar

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
+import { faArrowRightFromBracket, faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Menu, MenuItem, Nav } from './styles';
+import {
+  Menu, MenuItem, Nav, MobileMenuIcon,
+} from './styles';
 
-const Navbar = () => (
-  <Nav>
-    <h1>GAMETRAIL</h1>
-    <Menu>
-      <MenuItem href="/home">
-        <h4>Inicio</h4>
-      </MenuItem>
-      <MenuItem href="/games">
-        <h4>Juegos</h4>
-      </MenuItem>
-      <MenuItem href="/trail/create">
-        <h4>Crear Trail</h4>
-      </MenuItem>
-      <MenuItem href="/auth/logout">
-        <FontAwesomeIcon icon={faArrowRightFromBracket} />
-      </MenuItem>
-    </Menu>
-  </Nav>
-);
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <Nav>
+      <h1>GAMETRAIL</h1>
+      <MobileMenuIcon onClick={toggleMenu}>
+        <FontAwesomeIcon icon={faBars} />
+      </MobileMenuIcon>
+      {showMenu ? (
+        <Menu>
+          <MenuItem href="/home">
+            <h4>Inicio</h4>
+          </MenuItem>
+          <MenuItem href="/games">
+            <h4>Juegos</h4>
+          </MenuItem>
+          <MenuItem href="/trail/create">
+            <h4>Crear Trail</h4>
+          </MenuItem>
+          <MenuItem href="/auth/logout">
+            <FontAwesomeIcon icon={faArrowRightFromBracket} />
+          </MenuItem>
+        </Menu>
+      ) : null}
+    </Nav>
+  );
+};
 
 export default Navbar;
--- src/components/Layout/Navbar/styles.ts
+++ src/components/Layout/Navbar/styles.ts
@@ -2,34 +2,77 @@ import styled from 'styled-components';
 import Link from 'next/link';
 
 export const Nav = styled.nav`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
-    height: 60px;
-    padding: 0 135px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 60px;
+  padding: 0 135px;
+  background-color: ${({ theme }) => theme.nord.gray1};
+  color: ${({ theme }) => theme.nord.white0};
+  box-shadow: 0 0 10px ${({ theme }) => theme.nord.gray2};
 
-    background-color: ${({ theme }) => theme.nord.gray1};
-    color: ${({ theme }) => theme.nord.white0};
-    box-shadow: 0 0 10px ${({ theme }) => theme.nord.gray2};
-
-    @media (max-width: 768px) {
-        padding: 0 20px;
-    }
+  @media (max-width: 768px) {
+    padding: 0 20px;
+    flex-direction: column;
+    justify-content: center;
+    height: auto;
+  }
 `;
 
 export const Menu = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100%;
+    margin-top: 20px;
+    padding: 0;
+    background-color: ${({ theme }) => theme.nord.gray1};
+    color: ${({ theme }) => theme.nord.white0};
+  }
 `;
 
 export const MenuItem = styled(Link)`
-    display: flex;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  padding: 0 10px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.nord.gray3};
+    height: 60px;
+  }
+
+  @media (max-width: 768px) {
+    width: auto;
+    height: auto;
+    margin: 5px 0;
+    padding: 10px 20px;
+    border-radius: 5px;
+    text-align: center;
+  }
+`;
+
+export const MobileMenuIcon = styled.div`
+  display: flex;
+
+  @media (max-width: 768px) {
+    float: right;
     align-items: center;
     justify-content: center;
-    width: 100%;
-    height: 100%;
-    padding: 0 10px;
+    width: 40px;
+    height: 40px;
     cursor: pointer;
+    margin-left: auto;
+
+    svg {
+      font-size: 1.5rem;
+    }
+  }
 `;

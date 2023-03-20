88df9ea860eca15380edd2ed3a25b2837a08f7d2
jvegax <toledo.utrera@hotmail.com>
2023-03-20 01:35:36 +0100

    fix: hydratation due to responsive navbar

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,78 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { faArrowRightFromBracket, faBars } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  Menu, MenuItem, Nav, MobileMenuIcon, ResponsiveNavbar,
-} from './styles';
+import { Menu, MenuItem, Nav } from './styles';
 
-const Navbar = () => {
-  const [showMenu, setShowMenu] = useState(false);
-  const [width, setWidth] = useState(typeof window !== 'undefined' ? window.innerWidth : 0);
-
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (width > 768) {
-      setShowMenu(false);
-    }
-  }, [width]);
-
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
-
-  return (
-    <Nav>
-      <ResponsiveNavbar>
-        <h1>GAMETRAIL</h1>
-        {width <= 768 && (
-          <MobileMenuIcon showMenu={showMenu} onClick={toggleMenu}>
-            <FontAwesomeIcon icon={faBars} />
-          </MobileMenuIcon>
-        )}
-      </ResponsiveNavbar>
-      {width > 768 ? (
-        <Menu>
-          <MenuItem href="/home">
-            <h4>Inicio</h4>
-          </MenuItem>
-          <MenuItem href="/games">
-            <h4>Juegos</h4>
-          </MenuItem>
-          <MenuItem href="/trail/create">
-            <h4>Crear Trail</h4>
-          </MenuItem>
-          <MenuItem href="/auth/logout">
-            <FontAwesomeIcon icon={faArrowRightFromBracket} />
-          </MenuItem>
-        </Menu>
-      ) : (
-        showMenu && (
-          <Menu>
-            <MenuItem href="/home">
-              <h4>Inicio</h4>
-            </MenuItem>
-            <MenuItem href="/games">
-              <h4>Juegos</h4>
-            </MenuItem>
-            <MenuItem href="/trail/create">
-              <h4>Crear Trail</h4>
-            </MenuItem>
-            <MenuItem href="/auth/logout">
-              <FontAwesomeIcon icon={faArrowRightFromBracket} />
-            </MenuItem>
-          </Menu>
-        )
-      )}
-    </Nav>
-  );
-};
+const Navbar = () => (
+  <Nav>
+    <h1>GAMETRAIL</h1>
+    <Menu>
+      <MenuItem href="/home">
+        <h4>Inicio</h4>
+      </MenuItem>
+      <MenuItem href="/games">
+        <h4>Juegos</h4>
+      </MenuItem>
+      <MenuItem href="/trail/create">
+        <h4>Crear Trail</h4>
+      </MenuItem>
+      <MenuItem href="/auth/logout">
+        <FontAwesomeIcon icon={faArrowRightFromBracket} />
+      </MenuItem>
+    </Menu>
+  </Nav>
+);
 
 export default Navbar;
--- src/components/Layout/Navbar/styles.ts
+++ src/components/Layout/Navbar/styles.ts
@@ -2,85 +2,34 @@ import styled from 'styled-components';
 import Link from 'next/link';
 
 export const Nav = styled.nav`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  height: 70px;
-  padding: 0 135px;
-  background-color: ${({ theme }) => theme.nord.gray1};
-  color: ${({ theme }) => theme.nord.white0};
-  box-shadow: 0 0 10px ${({ theme }) => theme.nord.gray2};
-  @media (max-width: 768px) {
-    padding: 0 20px;
-    flex-direction: column;
-    justify-content: center;
-    height: auto;
-  }
-`;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 60px;
+    padding: 0 135px;
 
-export const ResponsiveNavbar = styled.div`
-  @media (max-width: 768px) {
-      padding: 0 20px;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      height: auto;
+    background-color: ${({ theme }) => theme.nord.gray1};
+    color: ${({ theme }) => theme.nord.white0};
+    box-shadow: 0 0 10px ${({ theme }) => theme.nord.gray2};
+
+    @media (max-width: 768px) {
+        padding: 0 20px;
     }
 `;
 
 export const Menu = styled.div`
-  display: flex;
-  align-items: center;
-  @media (max-width: 768px) {
-    flex-direction: column;
+    display: flex;
     align-items: center;
-    width: 100%;
-    background-color: ${({ theme }) => theme.nord.gray1};
-    color: ${({ theme }) => theme.nord.white0};
-  }
+    justify-content: center;
 `;
 
 export const MenuItem = styled(Link)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-  padding: 0 10px;
-  cursor: pointer;
-  &:hover {
-    background-color: ${({ theme }) => theme.nord.gray3};
-    height: 70px;
-  }
-  @media (max-width: 768px) {
-    width: auto;
-    height: auto;
-    margin: 5px 0;
-    padding: 10px 20px;
-    border-radius: 5px;
-    text-align: center;
-  }
-`;
-
-interface MobileMenuIconProps {
-  showMenu: boolean;
-}
-
-export const MobileMenuIcon = styled.div<MobileMenuIconProps>`
-  display: flex;
-  @media (max-width: 768px) {
-    float: right;
+    display: flex;
     align-items: center;
     justify-content: center;
-    width: 40px;
-    height: 40px;
+    width: 100%;
+    height: 100%;
+    padding: 0 10px;
     cursor: pointer;
-    margin-left: auto;
-    transition: transform 0.3s ease-in-out;
-    transform: ${(props) => (props.showMenu ? 'rotate(90deg)' : 'rotate(0)')};
-    svg {
-      font-size: 1.5rem;
-    }
-  }
 `;

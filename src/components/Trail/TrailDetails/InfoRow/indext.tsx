0598b7c1a8072321c6cf2ffbd4f713e7f13c890e
Diego <diewentdiaz@gmail.com>
2023-03-27 01:04:43 +0200

    chore: prepared button for join with users

--- src/components/Trail/TrailDetails/InfoRow/indext.tsx
+++ src/components/Trail/TrailDetails/InfoRow/indext.tsx
@@ -1,6 +1,7 @@
 import type { FC } from 'react';
-import React from 'react';
+import React, { useState } from 'react';
 import DateInfo from '@/components/Trail/TrailDetails/InfoRow/DateInfo';
+import { useGameTrail } from '@/hooks';
 import type { Trail as TrailData } from '@/models/Trail/types';
 import {
   InfoRow, JoinButton, JoinContainer, JoinPlayersCount,
@@ -10,27 +11,81 @@ export type Props = {
   trailData: TrailData;
 };
 
-const PlusInfoRow: FC<Props> = ({ trailData }) => (
-  <InfoRow>
-    <DateInfo dateStart={trailData.startDate} dateEnd={trailData.finishDate} />
-    <JoinContainer>
-      <JoinButton>
+const PlusInfoRow: FC<Props> = ({ trailData }) => {
+  const { user, token } = useGameTrail();
+  const [isJoined, setIsJoined] = useState(false);
+
+  if (user) {
+    trailData.users.forEach((userTrail) => {
+      if (userTrail.id === user.id) {
+        setIsJoined(true);
+      }
+    });
+  }
+
+  const handleJoin = () => {
+    const requestData = {
+      trailId: trailData.id,
+      userId: user?.id.toString(),
+    };
+    try {
+      fetch('https://gametrail-backend-production.up.railway.app/api/trail/join', {
+        method: 'POST',
+        body: JSON.stringify(requestData),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Token ${token}`,
+        },
+      }).then((r) => r);
+    } catch (error) {
+      throw new Error();
+    }
+  };
+
+  let joinButton: JSX.Element;
+  if (user && trailData.users.length < trailData.maxPlayers) {
+    if (isJoined) {
+      joinButton = (
+        <JoinButton>
+          <p>UNIDO</p>
+        </JoinButton>
+      );
+    } else {
+      joinButton = (
+        <JoinButton onClick={handleJoin}>
+          <p>UNIRSE</p>
+        </JoinButton>
+      );
+    }
+  } else {
+    joinButton = (
+      <JoinButton style={{ visibility: 'hidden' }}>
         <p>UNIRSE</p>
       </JoinButton>
-      <JoinPlayersCount>
-        <p>
-          ‍🙍
-        </p>
-        <p>
-          {trailData.users.length}
-          /
-          {trailData.maxPlayers}
-        </p>
-      </JoinPlayersCount>
+    );
+  }
 
-    </JoinContainer>
+  return (
+    <InfoRow>
+      <DateInfo dateStart={trailData.startDate} dateEnd={trailData.finishDate} />
+      <JoinContainer>
+        {joinButton}
 
-  </InfoRow>
-);
+        <JoinPlayersCount>
+          <p>
+            ‍🙍
+          </p>
+          <p>
+            {trailData.users.length}
+            /
+            {trailData.maxPlayers}
+          </p>
+        </JoinPlayersCount>
+
+      </JoinContainer>
+
+    </InfoRow>
+  );
+};
 
 export default PlusInfoRow;

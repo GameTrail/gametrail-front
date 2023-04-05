e03406d019cc06449620f5a72541e886f5d2084c
jvegax <toledo.utrera@hotmail.com>
2023-04-05 20:35:37 +0200

    fix: loading state on home page

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,23 +1,27 @@
 import type { FC } from 'react';
 import React from 'react';
 import { TrailGang, UnkownData, RecentGames } from '@/components/Home';
+import LoadingSpinner from '@/components/LoadingSpinner';
 import type { Game } from '@/models/Game/types';
 import type { RandomTip } from '@/models/RandomTip/types';
 import type { Trail as TrailGangProps } from '@/models/Trail/types';
 import { Container } from './styled';
 
 export type Props = {
-  trailGang: TrailGangProps;
-  recentGames: Game[];
-  unknownData: RandomTip[];
+  trailGang: TrailGangProps | null;
+  recentGames: Game[] | null;
+  unknownData: RandomTip[] | null;
 };
 
-const Home:FC<Props> = ({ trailGang, recentGames, unknownData }) => (
-  <Container>
-    <TrailGang trailGang={trailGang} />
-    <RecentGames recentGames={recentGames} />
-    <UnkownData unknownData={unknownData} />
-  </Container>
-);
+const Home:FC<Props> = ({ trailGang, recentGames, unknownData }) => {
+  if (!trailGang || !recentGames || !unknownData) return <LoadingSpinner />;
+  return (
+    <Container>
+      <TrailGang trailGang={trailGang} />
+      <RecentGames recentGames={recentGames} />
+      <UnkownData unknownData={unknownData} />
+    </Container>
+  );
+};
 
 export default Home;

208d2d2521812920cf2cb99dc5144457be66298d
LuisUsrDev <luis.311294@gmail.com>
2023-04-08 18:49:28 +0200

    chore: eliminados atributos innecesarios en el style de CommentBox

--- src/components/Comments/CommentsSection/CommentBox/styles.ts
+++ src/components/Comments/CommentsSection/CommentBox/styles.ts
@@ -11,7 +11,6 @@ export const CommentContainer = styled.div`
 export const AvatarContainer = styled.div`
     flex-basis: 10%;
     aling-items: center;
-    background-color: ${({ theme }) => theme.nord.gray1};
     img {
         border-radius: 50%;
         margin-left: 0.5rem;
@@ -22,13 +21,9 @@ export const CommentContentContainer = styled.div`
     flex-basis: 90%;
     color: ${({ theme }) => theme.nord.white1};
     font-size: 2rem;
-    background-color: ${({ theme }) => theme.nord.gray1};
 `;
 
 export const CommentUsernameContainer = styled.div`
-    flex-basis: 90%;
-    color: ${({ theme }) => theme.nord.white1};
-    background-color: ${({ theme }) => theme.nord.gray1};
     font-size: 1rem;
     margin-bottom: 1rem;
     @media (max-width: 768px) {
@@ -37,8 +32,5 @@ export const CommentUsernameContainer = styled.div`
 `;
 
 export const CommentTextContainer = styled.div`
-    flex-basis: 90%;
-    color: ${({ theme }) => theme.nord.white1};
-    background-color: ${({ theme }) => theme.nord.gray1};
     font-size: 1.2rem;
 `;

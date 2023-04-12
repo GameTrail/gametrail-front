45bcc0d3260d0c6cce54ab36a12b0faba9149cdf
jvegax <toledo.utrera@hotmail.com>
2023-04-12 22:00:08 +0200

    chore: trail pagination

--- src/containers/TrailList/index.tsx
+++ src/containers/TrailList/index.tsx
@@ -1,42 +1,84 @@
 import React, { useEffect, useState, memo } from 'react';
+import Error from '@/components/Error';
 import LoadingSpinner from '@/components/LoadingSpinner';
+import PaginationCard from '@/components/PaginationCard';
 import type { Trail } from '@/models/Trail/types';
 import { Container, Title, TrailListContainer } from './styles';
 import TrailCard from './TrailCard';
 
+const API_URL = 'https://gametrail-backend-production-8fc0.up.railway.app/api/getTrail/';
 const TrailList = () => {
-  const [trails, setTrails] = useState<Trail[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
+  const [trails, setTrails] = useState<Trail[] | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [pages, setPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const handleRenderTrails = () => trails.map((trail) => (
-    <TrailCard key={trail.id} trail={trail} />
-  ));
+  const handleRenderTrails = () => {
+    if (!trails) return null;
+    return trails.map((trail) => (
+      <TrailCard key={trail.id} trail={trail} />
+    ));
+  };
+
+  const handleSetPages = (trailsCount: number) => {
+    setPages(Math.ceil(trailsCount / 16));
+  };
+
+  const handlePagination = async (page: number) => {
+    setLoading(true);
+    try {
+      const searchUrl = `${API_URL}?page=${page}`;
+      const response = await fetch(searchUrl);
+      const data = await response.json();
+
+      setTrails(data.results);
+      setCurrentPage(page);
+      setError(false);
+    } catch (err) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     const fetchTrails = async () => {
       setLoading(true);
       try {
-        const response = await fetch('https://gametrail-backend-production-8fc0.up.railway.app/api/getTrail/');
+        const response = await fetch(API_URL);
         const data = await response.json();
+
+        if (!response.ok) {
+          setError(true);
+          return;
+        }
         setTrails(data.results);
-        setError(null);
+        handleSetPages(data.count);
+        setError(false);
       } catch (err) {
-        setError('Ops! Ha ocurrido un error inesperado.');
+        setError(true);
       } finally {
         setLoading(false);
       }
     };
     fetchTrails();
   }, []);
+
   if (loading) return <LoadingSpinner />;
-  if (error) return <p>{error}</p>;
+  if (error) return <Error />;
+
   return (
     <Container>
       <Title>Listado de Trails</Title>
       <TrailListContainer>
         {handleRenderTrails()}
       </TrailListContainer>
+      <PaginationCard
+        pages={pages}
+        currentPage={currentPage}
+        handlePagination={handlePagination}
+      />
     </Container>
   );
 };

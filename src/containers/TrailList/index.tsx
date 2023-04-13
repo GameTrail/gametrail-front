951e47df220cd99ebda961e82630568317448781
Anton1o007 <anto.m2110@gmail.com>
2023-04-13 21:33:02 +0200

    chore:avances en la busqueda de trails

--- src/containers/TrailList/index.tsx
+++ src/containers/TrailList/index.tsx
@@ -1,9 +1,13 @@
+import type { ChangeEvent } from 'react';
 import React, { useEffect, useState, memo } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
 import Error from '@/components/Error';
 import LoadingSpinner from '@/components/LoadingSpinner';
 import PaginationCard from '@/components/PaginationCard';
 import type { Trail } from '@/models/Trail/types';
-import { Container, Title, TrailListContainer } from './styles';
+import {
+  Container, Input, Title, TrailListContainer,
+} from './styles';
 import TrailCard from './TrailCard';
 
 const API_URL = 'https://gametrail-backend-production-8fc0.up.railway.app/api/getTrail/';
@@ -11,6 +15,7 @@ const TrailList = () => {
   const [trails, setTrails] = useState<Trail[] | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [searchQuery, setSearchQuery] = useState<string>('');
   const [pages, setPages] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -19,6 +24,10 @@ const TrailList = () => {
     return trails.map((trail) => (
       <TrailCard key={trail.id} trail={trail} />
     ));
+  };
+
+  const handleUpdateSearchQuery = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
   };
 
   const handleSetPages = (trailsCount: number) => {
@@ -42,28 +51,25 @@ const TrailList = () => {
     }
   };
 
-  useEffect(() => {
-    const fetchTrails = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch(API_URL);
-        const data = await response.json();
+  const debounceSearch = useDebouncedCallback(async (searchTerm: string) => {
+    setLoading(true);
+    try {
+      const searchUrl = searchTerm.length > 2 ? `${API_URL}?search=${searchTerm}` : API_URL;
+      const response = await fetch(searchUrl);
+      const data = await response.json();
+      setTrails(data.results);
+      handleSetPages(data.count);
+      setError(false);
+    } catch (err) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }, 750);
 
-        if (!response.ok) {
-          setError(true);
-          return;
-        }
-        setTrails(data.results);
-        handleSetPages(data.count);
-        setError(false);
-      } catch (err) {
-        setError(true);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchTrails();
-  }, []);
+  useEffect(() => {
+    debounceSearch(searchQuery);
+  }, [debounceSearch, searchQuery]);
 
   if (loading) return <LoadingSpinner />;
   if (error) return <Error />;
@@ -71,6 +77,7 @@ const TrailList = () => {
   return (
     <Container>
       <Title>Listado de Trails</Title>
+      <Input type="text" value={searchQuery} onChange={handleUpdateSearchQuery} placeholder="Buscar..." />
       <TrailListContainer>
         {handleRenderTrails()}
       </TrailListContainer>

096307ad092bd1a5a6ee754734c13b1f4ab965b1
Diego <diewentdiaz@gmail.com>
2023-04-14 14:54:04 +0200

    chore: Added ChangeLanguage to small Navbar

--- src/components/Layout/Navbar/SmallMenu.tsx
+++ src/components/Layout/Navbar/SmallMenu.tsx
@@ -2,7 +2,9 @@ import type { FC } from 'react';
 import React from 'react';
 import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import LanguagesSelector from '@/components/LanguagesSelector';
 import type { UserCookie } from '@/components/Login/LoginComponent/types';
+import useLanguage from '@/i18n/hooks';
 import {
   Avatar,
   LogoutItem, MenuItem, Premium,
@@ -13,34 +15,38 @@ export type Props = {
   handleStripeCheckout: (event: any) => void;
 };
 
-const SmallMenu: FC<Props> = ({ userCookie, handleStripeCheckout }) => (
-  <>
-    <MenuItem href="/home">
-      <h4>Inicio</h4>
-    </MenuItem>
-    <MenuItem href="/games">
-      <h4>Juegos</h4>
-    </MenuItem>
-    {userCookie && (
-      <>
-        <MenuItem href="/trail/create">
-          <h4>Crear Trail</h4>
-        </MenuItem>
-        <MenuItem href={`/user/${userCookie?.id}`}>
-          <Avatar src={userCookie.avatar} alt="No hay imagen" />
-        </MenuItem>
-        {userCookie?.plan === 'STANDARD' && (
+const SmallMenu: FC<Props> = ({ userCookie, handleStripeCheckout }) => {
+  const { t } = useLanguage();
+  return (
+    <>
+      <LanguagesSelector isNavbar />
+      <MenuItem href="/home">
+        <h4>{t('home')}</h4>
+      </MenuItem>
+      <MenuItem href="/games">
+        <h4>{t('games')}</h4>
+      </MenuItem>
+      {userCookie && (
+        <>
+          <MenuItem href="/trail/create">
+            <h4>{t('create_trail')}</h4>
+          </MenuItem>
+          <MenuItem href={`/user/${userCookie?.id}`}>
+            <Avatar src={userCookie.avatar} alt="No hay imagen" />
+          </MenuItem>
+          {userCookie?.plan === 'STANDARD' && (
           <Premium onClick={handleStripeCheckout}>
             <h4>Premium</h4>
           </Premium>
-        )}
+          )}
 
-        <LogoutItem href="/auth/logout">
-          <FontAwesomeIcon icon={faArrowRightFromBracket} />
-        </LogoutItem>
-      </>
-    )}
-  </>
-);
+          <LogoutItem href="/auth/logout">
+            <FontAwesomeIcon icon={faArrowRightFromBracket} />
+          </LogoutItem>
+        </>
+      )}
+    </>
+  );
+};
 
 export default SmallMenu;

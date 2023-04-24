546b9fed368b7273d87c10ef5d27e193f928aa94
joscacgom <joscacgom@alum.us.es>
2023-04-24 20:17:58 +0200

    test: userdata testing

---
+++ __tests__/UserData/UserTrailList.test.tsx
@@ -0,0 +1,39 @@
+import { ThemeProvider } from 'styled-components';
+import {
+  render, fireEvent,
+} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { useRouter } from 'next/router';
+import UserTrailList from '@/components/UserDetails/UserTrailList';
+import { MOCK_TRAIL_LIST } from '@/models/Trail/mock';
+import theme from '@/theme';
+import '@/i18n';
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn().mockReturnValue({ push: jest.fn() }),
+}));
+
+describe('UserTrailList', () => {
+  const trailList = MOCK_TRAIL_LIST;
+
+  it('renders a list of trails', () => {
+    const { getByText } = render(
+      <ThemeProvider theme={theme}>
+        <UserTrailList trailList={trailList} />
+      </ThemeProvider>,
+    );
+    expect(getByText('Trail de Jacky6868')).toBeInTheDocument();
+    expect(getByText('Trail de Joscacgom2')).toBeInTheDocument();
+  });
+
+  it('navigates to trail details when a trail is clicked', () => {
+    const { getByText } = render(
+      <ThemeProvider theme={theme}>
+        <UserTrailList trailList={trailList} />
+      </ThemeProvider>,
+    );
+    const trailItem = getByText('Trail de Jacky6868');
+    fireEvent.click(trailItem);
+    expect(useRouter().push).toHaveBeenCalledWith('/trail/[id]', '/trail/1');
+  });
+});

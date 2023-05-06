5322facad5b22268fb37bf40068a05a7a846062f
jvegax <toledo.utrera@hotmail.com>
2023-05-06 11:26:13 +0200

    chore: add meta descriptions on Head

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react';
 import React from 'react';
+import Head from 'next/head';
 import Footer from './Footer';
 import Navbar from './Navbar';
 import { Main } from './styles';
@@ -7,6 +8,36 @@ import type { Props } from './types';
 
 const Layout: FC<Props> = ({ children }) => (
   <>
+    <Head>
+      <title>GameTrail - Limitless gaming</title>
+      <meta name="description" content="GameTrail is a game platform that unite gamers into one community. We provide a platform for gamers to share their gaming experience and to find new friends." />
+      <link rel="icon" href="/brand/favicon.ico" />
+      <link rel="manifest" href="/manifest.json" />
+    </Head>
+    <Head>
+
+      <meta name="description" content="GameTrail is a game platform that unite gamers into one community. We provide a platform for gamers to share their gaming experience and to find new friends." lang="en" />
+
+      <meta name="description" content="GameTrail es una plataforma de videojuegos que une jugadores y comunidad. GameTrail ofrece a los gamers un lugar donde compartir sus experiencias de juego y encontrar nuevos compañeros de juego" lang="es" />
+
+      <meta name="keywords" content="game, games, videogames, trail, gametrail, game-trail" />
+
+      <meta name="author" content="GameTrail" />
+
+      <meta name="robots" content="index, follow" />
+
+      <meta property="og:title" content="GameTrail 🎮 Limitless gaming" />
+
+      <meta property="og:description" content="Join gamers and start playing together with GameTrail. Live chat, Trails, Leaderboard and much more. Join now" lang="en" />
+
+      <meta property="og:url" content="https://www.gametrail.vercel.app" />
+
+      <meta property="og:image" content="public/brand/gametrail-logo.png" />
+
+      <link rel="icon" href="/brand/favicon.ico" />
+
+      <link rel="manifest" href="/manifest.json" />
+    </Head>
     <Navbar />
     <Main>
       {children}

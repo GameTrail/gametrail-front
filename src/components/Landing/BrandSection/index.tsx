abca1b92c3c3a1f9a247653260e05f4938f2bd4c
Josmarlop16 <josmarlop16@alum.us.es>
2023-05-06 11:45:26 +0200

    fix: rrss logo fixed

--- src/components/Landing/BrandSection/index.tsx
+++ src/components/Landing/BrandSection/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { InstaLottie, TwitterLottie, TiktokLottie } from '@/components/Lotties';
-import { Container, Social } from './styles';
+import { Container, Social, SocialTwitter } from './styles';
 
 const BrandSection = () => (
   <Container>
     <Social href="https://www.instagram.com/gametrail.app/">
       <InstaLottie />
     </Social>
-    <Social href="https://twitter.com/GameTrail_Esp">
+    <SocialTwitter href="https://twitter.com/GameTrail_Esp">
       <TwitterLottie />
-    </Social>
+    </SocialTwitter>
     <Social href="https://www.tiktok.com/@gametrail01">
       <TiktokLottie />
     </Social>

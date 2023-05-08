f03010d84a764c66dcc8beee881839d74ac92ad3
joscacgom <joscacgom@alum.us.es>
2023-05-08 21:34:08 +0200

    fix: tests fixed

--- __tests__/Trail/TrailDetails.test.tsx
+++ __tests__/Trail/TrailDetails.test.tsx
@@ -1,20 +1,24 @@
 import { ThemeProvider } from 'styled-components';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import type { NextRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import TrailDetails from '@/containers/Trail/TrailDetails';
 import { MOCK_TRAIL } from '@/models/Trail/mock';
 import theme from '@/theme';
 import '@/i18n';
 
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(),
+}));
+useRouter as jest.Mock<NextRouter>;
+
 describe('GameData component', () => {
   it('renders trail details correctly', () => {
     render(
-      <MemoryRouter>
-        <ThemeProvider theme={theme}>
-          <TrailDetails trailData={MOCK_TRAIL} />
-        </ThemeProvider>
-      </MemoryRouter>,
+      <ThemeProvider theme={theme}>
+        <TrailDetails trailData={MOCK_TRAIL} />
+      </ThemeProvider>,
     );
     expect(screen.getByText('2023-03-31')).toBeInTheDocument();
     expect(screen.getByText('2023-04-09')).toBeInTheDocument();
